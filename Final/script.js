6173dc10d33081c774e8a361ef2ad2469d71256e
WaiYanMoeMyint <wymoemyint4523@gmail.com>
2022-04-13 23:40:39 +0630

    Update nav sub section carousel jquery

--- Final/script.js
+++ Final/script.js
@@ -1,13 +1,5 @@
-var swiper = new Swiper(".mySwiper", {
-   cssMode: true,
-   navigation: {
-     nextEl: ".swiper-button-next",
-     prevEl: ".swiper-button-prev",
-   },
-   pagination: {
-     el: ".swiper-pagination",
-   },
-   loop:true,
-   mousewheel: true,
-   keyboard: true,
- });
+$('.carousel').carousel({
+  interval: 6000,
+  wrap:true,
+  keyboard:true,
+});
