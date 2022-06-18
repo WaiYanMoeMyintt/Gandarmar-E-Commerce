65308b94f063f93f41f2b79b16464fec9909e316
WaiYanMoeMyint <wymoemyint4523@gmail.com>
2022-06-18 14:59:33 +0630

    Complete product js section

--- Final/script.js
+++ Final/script.js
@@ -5,6 +5,11 @@ const content = document.querySelectorAll(".content-product");
 
 btn.forEach((tabs,index)=>{
    tabs.addEventListener("click",()=>{
+      //add background color and remove class
+      btn.forEach((e)=> e.classList.remove("banner-active"));
+      btn[index].classList.add("banner-active");
+     
+      //add product active class and remove this class
       content.forEach((c)=>c.classList.remove("product-is-active"));
       content[index].classList.add("product-is-active");
    })
